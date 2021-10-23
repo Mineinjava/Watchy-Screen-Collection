497b2a4195c6879ca1bc6901cab4ba8d41ff857e
Mineinjava <65673396+Mineinjava@users.noreply.github.com>
2021-10-23 14:13:01 -0700

    Create StopWatch.h

---
+++ StopWatch/StopWatch.h
@@ -0,0 +1,12 @@
+
+#include "Screen.h"
+
+
+class StopWatch : public Screen {
+  void show();
+  void showMe();
+  void up();
+  void down();
+  void back();
+  String add_zero_before_int(int num);
+};
